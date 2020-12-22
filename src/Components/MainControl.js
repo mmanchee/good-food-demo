6da257661fd26cd9b26d666c41e684c117322223
Mike Manchee <mikemanchee@gmail.com>
2020-12-22 09:27:03 -0800

    learning about hooks

--- src/Components/MainControl.js
+++ src/Components/MainControl.js
@@ -1,15 +1,33 @@
 import React from 'react';
 
-class MainControl extends React.Component {
+const MainControl = () => {
+  const [page, setPage] = useState('MainMenu')
 
-  render() {
-    let currentVisibleState = <p>Hello World</p>
-    return (
-      <React.Fragment>
-        {currentVisibleState}
-      </React.Fragment>
-    )
+  const changePage = newPage => {
+    setPage(newPage);
   }
+
+  let currentVisibleState = null;
+  
+  switch (page) {
+    case 'MainMenu':
+      return currentVisibleState = <MainMenu onChangePage = {this.changePage}/>
+    case 'Login':
+      return currentVisibleState = <Login onChangePage = {this.changePage}/>
+    case 'Register':
+      return currentVisibleState = <Register onChangePage = {this.changePage}/>
+    case 'Login':
+      return currentVisibleState = <Login onChangePage = {this.changePage}/>
+    case 'Login':
+      return currentVisibleState = <Login onChangePage = {this.changePage}/>
+    default:
+      return currentVisibleState = <Landing onChangePage = {this.changePage}/>
+  }
+  return (
+    <React.Fragment>
+      {currentVisibleState}
+    </React.Fragment>
+  )
 }
 
 export default MainControl;
